4242a7b1ac2fc4840f935f6a3c2b7241026bf058
Jude Rizzo <jrizzo00@stuy.edu>
2019-04-01 16:34:30 -0400

    working on that linked list

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,7 @@
 import java.util.*;
-public class MyLinkedList{
+public class MyLinkedList<E>{
+private int size;
+private Node<E> start, end;
 
 private class Node<E>{
   public E val;
@@ -26,7 +28,109 @@ private class Node<E>{
 
 }
 
+public MyLinkedList(){
+ size = 0;
+
+}
 
 
+public void addFront(Node<E> leon){
+  if(size == 0) {
+    start = leon;
+    end = leon;
+    size++;
+    return;
+  }
+  start.setPrev(leon);
+  leon.setNext(start);
+  start = leon;
+  size ++;
+}
+
+public void addBack(Node<E> leon){
+  if(size == 0) {
+    start = leon;
+    end = leon;
+    size++;
+    return;
+  }
+  end.setNext(leon);
+  leon.setPrev(start);
+  end = leon;
+  size +=1;
+
+}
+
+
+public Node<E> removeFront(){
+  if(size == 0) { return null;}
+  (start.end).setPrev(null);
+  Node<E> val = start;
+  start = start.end;
+  size --;
+  return val;
+
+  }
+
+
+
+  public Node<E> removeEnd(){
+    if(size == 0) return null;
+    (end.start).setNext(null);
+    Node<E> val = end;
+    end = end.start;
+    size--;
+    return val;
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public void addFront(E leon){
+      Node n = new Node(leon);
+      addFront(n);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void addBack(E leon){
+      Node n = new Node(leon);
+      addBack(n);
+    }
+
+
+
+    public String toString(){
+      String ans = "[";
+      if(size == 0) return "[]";
+      Node<E> n = start;
+      while(n != this.end){
+        ans += n.toString();
+        n = n.end;
+      }
+
+      ans += this.end + "]";
+      return ans;
+    }
+
+
+    public void extend(MyLinkedList<E> other){
+      end.setNext(other.start);
+      (other.start).setPrev(end);
+      end = other.end;
+    }
+
+
+
+
+    public static void main(String[] args){
+      //start with empty linked MyLinkedList
+      MyLinkedList<Integer> leon = new MyLinkedList<Integer>();
+      System.out.println(leon.toString());
+
+      for(int i = 0; i < 3; i++){
+        leon.addBack(i);
+      }
+      System.out.println(leon.toString());
+
+    }
 
 }
