3e6889425cee27f12f40904d2c58ebd4680ca62b
Jude-Rizzo <jprizzo2020@gmail.com>
2019-04-02 11:47:25 -0400

    classwork

--- Driver.java
+++ Driver.java
@@ -3,7 +3,7 @@ public class Driver{
   //test MyLinkedList
   public static void main(String[] args){
     //start with empty linked MyLinkedList
-    MyLinkedList<Integer> leon = new MyLinkedList<Integer>();
+  /*  MyLinkedList<Integer> leon = new MyLinkedList<Integer>();
     System.out.println(leon.toString());
 
     for(int i = 0; i < 10; i++){
@@ -26,8 +26,15 @@ public class Driver{
   h.removeBack();
   System.out.println(h);
   leon.extend(h);
-  System.out.println(leon);
+  System.out.println(leon); */
+  //now testing RadixSort
 
+  //System.out.println(Radix.countDigits(10));
+//  System.out.println(Radix.countDigits(10000002) + "should be 8");
+  //System.out.println(Radix.getNthDigit(2,222));
+  System.out.println(Radix.getNthDigit(0,1111122223));
+  int[] data = {2,4,2,4,3,6,4,6,8,4,3,5,7,90,7,5,3,5,3,56,4,2,234,2,5,462456,2,7,456,34,5,5345,345,3456,2,6,2};
+  System.out.println(Radix.radixsort(data));
 
 
 }
--- Radix.java
+++ Radix.java
@@ -1,1 +1,41 @@
-public class 
+public class Radix{
+
+
+  public static int countDigits(int n){
+    int counter = 1;
+    while(n/10 > 0){
+      n = n/10;
+      counter ++;
+    }
+
+    return counter;
+  }
+
+  public static int getNthDigit(int n,int num){
+    int count = 0;
+
+    while(count < n){
+      num = num/10;
+      count++;
+    }
+    return  num%10;
+  }
+
+  //ABOVE STARTS AT 0 AND GOES RIGHT TO LEFT
+//only returns for sake of testing
+    @SuppressWarnings("unchecked")
+  public static MyLinkedList<Integer>[] radixsort(int[] data){
+    int largestLength = 0;
+    int digit = 0;
+    //start with only positives
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+    for(int num : data){
+      //now we gotta put them into their respective buckets
+      if(countDigits(num) > largestLength) largestLength = countDigits(num);
+      buckets[getNthDigit(0, num)].addBack(num);
+
+      }
+
+      return buckets;
+    }
+  }
