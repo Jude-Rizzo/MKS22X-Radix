1bdd7fca3bedb2bca714bd651431712464082278
Jude Rizzo <jrizzo00@stuy.edu>
2019-04-06 15:47:34 -0400

    beast

--- Radix.java
+++ Radix.java
@@ -65,54 +65,48 @@ public class Radix{
           }
         }
 
+        else{
+       for (int idx = 0; idx < data.length; idx++){
+         //get the digit
+         int num = temp.removeFront();
+         int digit = (num / base) % 10;
+         //System.out.println("pass #: " + i + " base: " + base + " num: " + num + " digit: " + digit);
+         //sort the positives
+         if (num >= 0){
+           //System.out.println("pass #: " + i + " base: " + base + " num: " + num + " bucket #: " + (digit + 10) + " digit: " + digit);
+           buckets[digit + 10].add(num);
+           //System.out.println("bucket: " + (digit+10) + " " + buckets[digit+10].toString());
+         }
+         //sort the negatives
+         else{
+           buckets[9 - digit].add(num);
+         }
+       }
+
 
         //new stuff
 
     //ok above sorts them by first digit but also gets the larges number of digits;
-    for(int i = 1; i < largestLength; i++){
-      //now we are swapping between 2 lists
-      if(i %2 == 1){
-        //this keeps track of which list were swapping into, if i is odd we swap into buckets2
-        buckets2 = new MyLinkedList[10];
-        //recreate buckets 2
-        for(int j = 0; j < buckets.length; j++){
-          //no we gotta go through buckets and put all the numb
-        if(buckets[j] != null){
-          for(int k = 0; k < buckets[j].size(); k++){
-
-            System.out.println(buckets[j].size());
-              System.out.println(buckets[j]);
-
-
-              Integer num = buckets[j].removeBack().value();
-              //System.out.println(num);
-              //this is the first element of buckets were gonna be looking at
-              if(buckets2[getNthDigit(i, num)] == null) buckets2[getNthDigit(i, num)] = new MyLinkedList<Integer>();
-              buckets2[getNthDigit(i, num)].addBack(getNthDigit(i, num));
-            }
-          }
+    temp.clear();
+        //copy from buckets to temp and extend
+        for (int j = 0; j < 20; j++){
+          temp.extend(buckets[j]);
+        }
+        //System.out.println("reorder #: " + (i+1) + " " + temp.toString());
+        //clear the buckets for another pass
+        for (MyLinkedList<Integer> m : buckets){
+          m.clear();
         }
       }
-
-      if(i % 2 == 0){
-        buckets = new MyLinkedList[10];
-        //recreate buckets 2
-        for(int j = 0; j < buckets2.length; j++){
-          //no we gotta go through buckets and put all the numb
-          for(int k = 0; k < buckets2[j].size(); k++){
-
-            int num = buckets2[j].removeFront().value();
-            //this is the first element of buckets were gonna be looking at
-            if(buckets[getNthDigit(i, num)] == null) buckets[getNthDigit(i, num)] = new MyLinkedList<Integer>();
-            buckets[getNthDigit(i, num)].addBack(getNthDigit(i, num));
-
-          }
-
-        }
-      }
+      //increase the base
+      base *= 10;
     }
-    if(largestLength % 2 == 1) return buckets;
-    else return buckets2;
+    //copy from linked list to original array
+    for (int i = 0; i < data.length; i++){
+      data[i] = temp.removeFront();
+    }
+  }
+}
 
 
   }
